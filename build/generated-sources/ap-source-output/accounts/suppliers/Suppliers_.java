58b8801758d519e345965279a166092042b12348
jikoyster <yu.jake@gmail.com>
2017-01-03 01:38:36 +0800

    created updated jar file

--- build/generated-sources/ap-source-output/accounts/suppliers/Suppliers_.java
+++ build/generated-sources/ap-source-output/accounts/suppliers/Suppliers_.java
@@ -2,10 +2,12 @@ package accounts.suppliers;
 
 import java.util.Date;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
+import transactions.orders.Orders;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-29T02:31:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-03T01:37:57")
 @StaticMetamodel(Suppliers.class)
 public class Suppliers_ { 
 
@@ -13,6 +15,7 @@ public class Suppliers_ {
     public static volatile SingularAttribute<Suppliers, String> address;
     public static volatile SingularAttribute<Suppliers, String> phone;
     public static volatile SingularAttribute<Suppliers, String> name;
+    public static volatile CollectionAttribute<Suppliers, Orders> ordersCollection;
     public static volatile SingularAttribute<Suppliers, String> fax;
     public static volatile SingularAttribute<Suppliers, Date> dateadded;
     public static volatile SingularAttribute<Suppliers, String> email;
